e11060f38efe6a4281e8748eaf47c9ba40283dc9
Mike McDonald <michael@castaspes.com>
2025-11-13 23:03:24 +0200

    starting the update ota

--- ZombieVerterDisplay.ino
+++ ZombieVerterDisplay.ino
@@ -12,7 +12,7 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include <AsyncElegantOTA.h>
+#include <ElegantOTA.h>
 #include <ArduinoJson.h>
 
 #define AP_SSID "ZombieDisplay"
@@ -328,7 +328,7 @@ void setup() {
       request->send(200);
     }, handleUpload);
 
-  AsyncElegantOTA.begin(&server);    // Start AsyncElegantOTA
+  ElegantOTA.begin(&server);    // Start ElegantOTA in async mode
   server.begin();
 
 
@@ -336,7 +336,7 @@ void setup() {
 
 void loop() {
 
-  AsyncElegantOTA.loop();
+  // ElegantOTA.loop() is not needed in async mode
   displayManager.Loop();
   inputManager.Loop();
 
